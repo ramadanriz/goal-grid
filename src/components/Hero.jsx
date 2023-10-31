7599c9eca87f5811cf5ad823365231bbc1888246
ramadanriz <zerorider96@gmail.com>
2023-10-31 10:21:05 +0700

    delete hero button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Stack, Text, Button } from "@chakra-ui/react";
+import { Container, Heading, Stack, Text } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
@@ -13,14 +13,6 @@ const Hero = () => {
         <Text color={"gray.500"} maxW={"3xl"}>
           Never miss a game. Never be out of touch with your team. Stay on top of every match and receive live updates in real-time. Get your Matchday Masterplan ready for every kickoff.
         </Text>
-        <Stack spacing={6} direction={"row"}>
-          <Button rounded={"full"} px={6} colorScheme={"orange"} bg={"orange.400"} _hover={{ bg: "orange.500" }}>
-            Get started
-          </Button>
-          <Button rounded={"full"} px={6}>
-            Learn more
-          </Button>
-        </Stack>
       </Stack>
     </Container>
   );
